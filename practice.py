97e0c48ccc808b2b8619ca163e9eac5129b9298c
Hamza <muhammadmhamza35@gmail.com>
2024-06-27 05:51:09 -0400

    finished assignment 2 - type decarations and conversions

--- practice.py
+++ practice.py
@@ -1,6 +1,17 @@
-#Practice wAssignment 1 - Ask three questions, person's name, university, gpa, then print a message concatenating all three variables.
+#Practice Assignment 1 - Ask three questions, person's name, university, gpa, then print a message concatenating all three variables.
+'''
 name = input('What is your name? ')
 uni =input('Which university do you go to? ')
 gpa = input('What is your gpa? ')
 
 print('My name is ' + name + ' and I go to ' + uni + ' where my gpa is ' + gpa)
+'''
+#---------------------------------------------------------------------------------------#
+
+#Ask a user their weight and convert it to kilograms and print
+weight_lb = float(input('What is your weight in pounds? '))
+weight_kg = weight_lb*0.45359237
+print('Your weight in kg is: ')
+print(round(weight_kg, 2))
+
+##
