3cd7925c88b580e0d2d4ed9457fa059091e6ec8f
Hamza <muhammadmhamza35@gmail.com>
2024-06-27 05:40:39 -0400

    finished assignment 1 -- input and print statements

---
+++ practice.py
@@ -0,0 +1,6 @@
+#Practice wAssignment 1 - Ask three questions, person's name, university, gpa, then print a message concatenating all three variables.
+name = input('What is your name? ')
+uni =input('Which university do you go to? ')
+gpa = input('What is your gpa? ')
+
+print('My name is ' + name + ' and I go to ' + uni + ' where my gpa is ' + gpa)
