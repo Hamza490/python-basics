6a10acdddd097dc9a3e31d0055126bb1d8934d9c
Hamza <muhammadmhamza35@gmail.com>
2024-06-27 14:14:07 -0400

    edited the assignment 3, i believe the issue is due to global and local issues relating to the rate variable

--- practice.py
+++ practice.py
@@ -21,22 +21,25 @@ print(round(weight_kg, 2))
 #Assignment 3 - A user is buying a home. Ask for the cost of the home and determine how much the user has to pay as their downpayment based off of their crdit score. A good credit score is 800 and aboe.
 
 housePrice = float(input('How much does your house cost? '))
-creditScore = float(input('What is your credit score? '))
+creditScore = int(input('What is your credit score? '))
 downPayment = 0.0
-rate = int(0.0)
+rate = float(0.0)
 
 def downpaymentPercentage (credit):
-  if creditScore >= 800:
-    rate == 0.1
-  elif creditScore < 800:
-    rate == 0.2
-  elif creditScore == 0:
-    rate == 0.0
+  print('hi')
+  rate = float(0.0)
+  if credit >= 800:
+    return rate == 0.1;
+    print(rate)
+  elif credit < 800 and credit > 0:
+    return rate == 0.2;
+    print(rate)
+  else:
+    return rate == 0.0;
+    print(rate)
+
+
+print(creditScore)
 rate = downpaymentPercentage(creditScore)
 print(rate)
 
-if rate > 0:
-  downPayment = rate*housePrice
-  print(downPayment)
-elif rate <= 0:
-  print('You do not qualify for a line of credit.')
