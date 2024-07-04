93f4c3d8a9507ff816a820d3e2ddb485317a9dea
Hamza <muhammadmhamza35@gmail.com>
2024-07-04 17:47:39 -0400

    made a simple number guessing game, it has a preset answer key and tries

---
+++ guessing-game.py
@@ -0,0 +1,13 @@
+secret_number = 8
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+  guess = int(input('Guess: '))
+  guess_count +=1
+  if guess == secret_number:
+    print('You won!')
+    break
+  else:
+    print('You guessed wrong.')
+else:
+  print('Sorry, you guessed 3 times and failed.')
