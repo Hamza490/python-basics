a51beb7782f83df59ffe093230f7108fb0a15068
Hamza <muhammadmhamza35@gmail.com>
2024-07-04 18:40:52 -0400

    Finished car-game v2 with status logic checking for whether the car has already started or stopped. This ensures you dont start the car twice or stop it twice.

--- car-game.py
+++ car-game.py
@@ -1,20 +1,39 @@
 user_input = input().lower()
 status = ""
 
-while user_input != 'quit':
+while True:
+
   if user_input == 'help':
+    status = 'help'
     print("""
 start - to start the car
 stop - to stop the car
 quit - to exit       
-    """)                       
+    """)           
+
   elif user_input == 'start':
+    status = 'start'
     print("Car has started...We're ready to go!")
+  
   elif user_input == 'stop':
+    status = 'stop'
     print("Your car has stopped. Nice breaking )")
+  
+  elif user_input == 'quit':
+    break
+  
   else:
     print("Sorry, I don't understand. Retype your command.")
 
-  user_input = input()
+  user_input = input().lower()
+  while status == user_input:
+    if status == 'start':
+      print('Car is already started. Input a different command.')
+      user_input = input().lower()
+    elif status == 'stop':
+      print('Car is already stopped. Input a different command.')
+      user_input = input().lower()
 
 print('Thank you for playing my game.')
+
+#Note: the status logic can be done with a do-while loop, and boolean, where there is a nested if/else if under the start and stop if/else if checking for the boolean status before printing the result.
